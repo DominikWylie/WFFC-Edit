bf357387aeaa3df6580e776da3c3722d9e5ae622
DOMINIK WYLIE <1703468@uad.ac.uk>
2025-04-29 20:21:51 +0100

    end of day commit, getting the mfc dialouge box done

---
+++ WOFFCEdit/ObjectViewer.cpp
@@ -0,0 +1,17 @@
+#include "ObjectViewer.h"
+BEGIN_MESSAGE_MAP(ObjectViewer, CDialogEx)
+//	ON_COMMAND(ID_BUTTON40001, &ObjectViewer::test)
+//	ON_UPDATE_COMMAND_UI(ID_BUTTON40001, &ObjectViewer::reetest)
+END_MESSAGE_MAP()
+
+
+//void ObjectViewer::test()
+//{
+	// TODO: Add your command handler code here
+//}
+
+
+//void ObjectViewer::reetest(CCmdUI* pCmdUI)
+//{
+	// TODO: Add your command update UI handler code here
+//}
---
+++ WOFFCEdit/ObjectViewer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "afxdialogex.h"
+#include "resource.h"
+
+class ObjectViewer : public CDialogEx
+{
+public:
+	DECLARE_MESSAGE_MAP()
+//	afx_msg void test();
+//	afx_msg void reetest(CCmdUI* pCmdUI);
+};
+
